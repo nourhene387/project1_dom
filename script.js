488bb0100218b9a1d0d3de71d392f1edeaea8ccc
nourhene387 <nourhenedhouibi9@gmail.com>
2024-09-25 09:56:38 +0200

    projet1_dom

---
+++ script.js
@@ -0,0 +1,18 @@
+
+/*document.addEventListener('DOMContentLoaded', () => {
+    var el = document.getElementById('quantity');
+    var quantityValue = parseInt(el.innerHTML,10);
+   
+ 
+    function clickinc() {
+        quantityValue++;
+        el.innerHTML = quantityValue; // Met à jour l'élément avec la nouvelle valeur
+        console.log(quantityValue); // Affiche la nouvelle valeur dans la console
+    }
+
+    // Ajoute un gestionnaire d'événements au bouton
+    document.getElementById('incrementBtn').addEventListener('click', clickinc);
+});*/
+
+
+
